ae90260693ef820f846d3112b262217f2010ceb0
Alexandru Chiriac <112606678+AlexandruChiriac98@users.noreply.github.com>
2024-08-24 19:16:27 +0200

    c

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+import importedUserList from './usersList.js';
+
 const { createApp } = Vue;
 
 const app = createApp({
     data() {
         return {
-            
+            importedUserList,
+            mainUser: {
+                name: 'Luffy',
+                avatar: 'img/Avatar-4.png'
+            },
+            notActive: true            
         }
     },
     methods: {
+        switchNot: function () {
+            if(this.notActive === true){
+                this.notActive = false
+            } else if (this.notActive === false) {
+                this.notActive = true
+            }
+        }
         
     },
     mounted() {
---
+++ js/usersList.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        name: 'Naruto Uzumaki',
+        avatar: 'img/Avatar-1.png',
+        messages: [
+            {
+                text: 'Ma sto Rasengaaaan',
+                date: '12/13/24',
+                mine: true
+            },
+            {
+                text: 'Ma sto GamuGamuuu',
+                date: '12/13/24',
+                mine: false
+            }
+
+        ]
+    },
+    {
+        name: 'Son Goku',
+        avatar: 'img/Avatar-2.png',
+        messages: [
+            {
+                text: 'Ma sto Sayan Gooood',
+                data: '12/13/24',
+                mine: true
+            },
+            {
+                text: 'Ma sto Gears Fiiiifth',
+                date: '12/13/24',
+                mine: false
+            }
+        ]
+    },
+    {
+        name: 'Inuyasha',
+        avatar: 'img/Avatar-3.png',
+        messages: [
+            {
+                text: 'Ma sta Putenzaa',
+                data: '12/13/24',
+                mine: true
+            },
+            {
+                text: 'Ma sta Putenzaa',
+                data: '12/13/24',
+                mine: false
+            }
+        ]
+    }
+]
